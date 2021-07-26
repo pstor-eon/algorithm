5786df336c1085d11cf10aad5b8ed9acc4dd89de
pstor <26815428+pstor-eon@users.noreply.github.com>
2021-07-27 03:20:50 +0900

    Add files via upload

---
+++ Baekjoon/01XX/1000/1000.cpp
@@ -0,0 +1,40 @@
+﻿/* #include <iostream>
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+
+	int A;
+	int B;
+
+	cin >> A;
+	cin >> B;
+
+	cout << A + B;
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+}
+*/
+
+
+// memory2020KB time0MS
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+}
