461371db193c48e0d12b17c049c29b87c9927167
pstor <26815428+pstor-eon@users.noreply.github.com>
2021-07-29 01:44:51 +0900

    Create 1001.cpp

---
+++ Baekjoon/01XX/1001/1001.cpp
@@ -0,0 +1,16 @@
+// memory 2020KB
+// time 0ms
+// language C++
+// code length 133B
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a - b << endl;
+
+    return 0;
+}
